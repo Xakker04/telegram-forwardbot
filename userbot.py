ff1e52f8dc3fcd7e3bd5991326bde7754f35d034
Xakker04 <157778986+Xakker04@users.noreply.github.com>
2025-05-26 09:28:46 +0500

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient, events
 
 # Bu yerga my.telegram.org dan olingan ma'lumotlarni yozasiz
-api_id = 25881372          # <- bu yerga sizning api_id
-api_hash = '966f84bbd225e891968ba786a4986535' # <- bu yerga sizning api_hash
+api_id = chat_id          # <- bu yerga sizning api_id
+api_hash = 'bot tokeni' # <- bu yerga sizning api_hash
 source_channel = 'https://t.me/kunuz'  # manba kanal @sizni_qiziqtirgan_kanal
 target_channel = 'https://t.me/UzTechWorlds'    # sizning kanal
 
